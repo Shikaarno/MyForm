51867005a498823042f161307efdb939f520918b
shikaarno <acidgumm@yandex.ru>
2022-08-17 14:52:41 +0300

    initial commit

---
+++ src/components/MyForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form action="">
+      <label for="cardNumber">Номер карты</label>
+      <input type="text" id="cardNumber">
+      <br>
+      <label for="cardDate">Действует до</label>
+      <input type="text" id="cardDate">
+      <br>
+      <label for="cardholderName">Имя держателя карты</label>
+      <input type="text" id="cardholderName">
+      <br>
+      <label for="cardCvv">CVV</label>
+      <input type="text" id="cardCvv">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyForm',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
